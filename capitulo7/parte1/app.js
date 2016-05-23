518858727b1a1d9b3bfda2370b4a73943fad15e2
Matheus Pessoa <matheusmpessoa17@gmail.com>
2016-05-23 18:29:50 -0300

    Capítulo 7 em andamento. Partes 1 / 2 / 3 resolvidas

---
+++ capitulo7/parte1/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("MyApp", []);
+
+app.controller("User", function ($scope) {
+    $scope.user = {};
+    $scope.wasSubmitted = false;
+    $scope.submit = function () {
+        $scope.wasSubmitted = true;
+    };
+});
