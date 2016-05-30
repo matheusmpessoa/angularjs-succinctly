7e1cd0047f9e63454998fcd1baa9f441136714e5
Matheus Pessoa <matheusmpessoa17@gmail.com>
2016-05-30 12:21:56 -0300

    Capítulo 8 corrigido

--- capitulo8/parte4/app.js
+++ capitulo8/parte4/app.js
@@ -1,41 +1,45 @@
 var app = angular.module("MyApp", []);
 
-app.factory("Item", function() {
+app.factory("Item", function () {
 
-  var items = [];
-  for (var i=0; i<50; i++) {
-    items.push({ id: i, name: "name "+ i, description: "description " + i });
-  }
-
-  return {
-    get: function(offset, limit) {
-      return items.slice(offset, offset+limit);
-    },
-    total: function() {
-      return items.length;
+    var items = [];
+    for (var i = 0; i < 50; i++) {
+        items.push({
+            id: i,
+            name: "name " + i,
+            description: "description " + i
+        });
     }
-  };
+
+    return {
+        get: function (offset, limit) {
+            return items.slice(offset, offset + limit);
+        },
+        total: function () {
+            return items.length;
+        }
+    };
 });
 
-app.controller("PaginationCtrl", function($scope, Item) {
+app.controller("PaginationCtrl", function ($scope, Item) {
 
-  $scope.itemsPerPage = 5;
-  $scope.currentPage = 0;
-  $scope.total = Item.total();
-  $scope.pagedItems = Item.get($scope.currentPage*$scope.itemsPerPage, $scope.itemsPerPage);
+    $scope.itemsPerPage = 5;
+    $scope.currentPage = 0;
+    $scope.total = Item.total();
+    $scope.pagedItems = Item.get($scope.currentPage * $scope.itemsPerPage, $scope.itemsPerPage);
 
-  $scope.loadMore = function() {
-    $scope.currentPage++;
-    var newItems = Item.get($scope.currentPage*$scope.itemsPerPage, $scope.itemsPerPage);
-    $scope.pagedItems = $scope.pagedItems.concat(newItems);
-  };
+    $scope.loadMore = function () {
+        $scope.currentPage++;
+        var newItems = Item.get($scope.currentPage * $scope.itemsPerPage, $scope.itemsPerPage);
+        $scope.pagedItems = $scope.pagedItems.concat(newItems);
+    };
 
-  $scope.nextPageDisabledClass = function() {
-    return $scope.currentPage === $scope.pageCount()-1 ? "disabled" : "";
-  };
+    $scope.nextPageDisabledClass = function () {
+        return $scope.currentPage === $scope.pageCount() - 1 ? "disabled" : "";
+    };
 
-  $scope.pageCount = function() {
-    return Math.ceil($scope.total/$scope.itemsPerPage);
-  };
+    $scope.pageCount = function () {
+        return Math.ceil($scope.total / $scope.itemsPerPage);
+    };
 
 });
--- capitulo8/parte5/app.js
+++ capitulo8/parte5/app.js
@@ -1,36 +1,42 @@
 var app = angular.module("MyApp", []);
 
-app.config(function($routeProvider) {
-  $routeProvider.
-    when("/home", { templateUrl: "home.html" }).
-    when("/page", { templateUrl: "page.html" }).
-    otherwise({ redirectTo: "/home" });
+app.config(function ($routeProvider) {
+    $routeProvider.
+    when("/home", {
+        templateUrl: "home.html"
+    }).
+    when("/page", {
+        templateUrl: "page.html"
+    }).
+    otherwise({
+        redirectTo: "/home"
+    });
 });
 
-app.factory("flash", function($rootScope) {
-  var queue = [];
-  var currentMessage = "";
+app.factory("flash", function ($rootScope) {
+    var queue = [];
+    var currentMessage = "";
 
-  $rootScope.$on("$routeChangeSuccess", function() {
-    currentMessage = queue.shift() || "";
-  });
+    $rootScope.$on("$routeChangeSuccess", function () {
+        currentMessage = queue.shift() || "";
+    });
 
-  return {
-    setMessage: function(message) {
-      queue.push(message);
-    },
-    getMessage: function() {
-      return currentMessage;
+    return {
+        setMessage: function (message) {
+            queue.push(message);
+        },
+        getMessage: function () {
+            return currentMessage;
+        }
+    };
+});
+
+app.controller("MyCtrl", function ($scope, $location, flash) {
+    $scope.flash = flash;
+    $scope.message = "Hello World";
+
+    $scope.submit = function (message) {
+        flash.setMessage(message);
+        $location.path("/page");
     }
-  };
-});
-
-app.controller("MyCtrl", function($scope, $location, flash) {
-  $scope.flash = flash;
-  $scope.message = "Hello World";
-
-  $scope.submit = function(message) {
-    flash.setMessage(message);
-    $location.path("/page");
-  }
 });
